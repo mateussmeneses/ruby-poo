833483ddcfbcd64d6845ff700cd82820034fa6b5
Mateus Meneses <mateusmeneses86@gmail.com>
2022-06-15 09:02:15 -0300

    Initial Commit

---
+++ módulos/pagamento.rb
@@ -0,0 +1,25 @@
+module Pagamento
+  PI = 3.14
+
+  def pagar(badeira, numero, valor)
+    "Pagando com o cartão #{badeira} Número #{numero}, o valor de R$#{valor}..."
+  end
+
+  class Visa
+    def pagando
+      "pagando..."
+    end
+  end
+endmodule Pagamento
+  PI = 3.14
+
+  def pagar(badeira, numero, valor)
+    "Pagando com o cartão #{badeira} Número #{numero}, o valor de R$#{valor}..."
+  end
+
+  class Visa
+    def pagando
+      "pagando..."
+    end
+  end
+end
